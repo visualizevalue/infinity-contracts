318cdc1930dd8f04f04dd1a02fdd26281591b7bd
jalil <jalil@1001.digital>
2023-07-05 16:15:57 +0200

    Adjust colors

--- colors.ts
+++ colors.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
-const COUNT = 15
-const START_HUE = 8
+const COUNT = 16
+const START_HUE = 176
 const HUE_INCREMENT = 360 / COUNT
 const SATS = [88, 88, 88, 88]
 const LUMS = [56, 60, 64, 72]
@@ -13,8 +13,6 @@ class Color {
 
 const rootBand = [...Array(COUNT)].map((_, i) => new Color(START_HUE + i * HUE_INCREMENT, SATS[0], LUMS[0]))
 
-console.log(rootBand)
-
 const colors: Color[] = []
 for (const color of rootBand) {
   colors.push(new Color(color.h, color.s, color.l))
@@ -23,10 +21,11 @@ for (const color of rootBand) {
   colors.push(new Color(color.h, SATS[3], LUMS[3]))
 }
 
-const SVG = `
-  <svg width="400" height="1500" viewBox="0 0 400 1600" fill="none" xmlns="http://www.w3.org/2000/svg">
+const SVG = `<svg width="400" height="1500" viewBox="0 0 400 1500">
     ${
-      colors.map((c, i) => `<rect width="100" height="100" x="${i % 4 * 100}" y="${Math.floor(i / 4) * 100}" fill="hsl(${c.h} ${c.s}% ${c.l}%)" />`)
+      colors
+        .map((c, i) => `<rect width="100" height="100" x="${i % 4 * 100}" y="${Math.floor(i / 4) * 100}" fill="hsl(${c.h} ${c.s}% ${c.l}%)" />`)
+        .join(`\n`)
     }
   </svg>
 `
