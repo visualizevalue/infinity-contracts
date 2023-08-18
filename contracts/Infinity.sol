fd606ff9ab8db3a42d4f8fc64bb1ce66f67d5e81
jalil <jalil@1001.digital>
2023-08-18 23:55:01 +0200

    Make naming of tokenId and id consistent

--- contracts/Infinity.sol
+++ contracts/Infinity.sol
@@ -36,7 +36,7 @@ contract Infinity is ERC1155 {
     /// @dev Emitted when minting a token with a message.
     event Message(address indexed from, address indexed to, uint256 indexed id, string message);
 
-    /// @dev Raised when the user isn't allowed to mint the given token ID.
+    /// @dev Raised when the user isn't allowed to mint the given token.
     error InvalidToken();
 
     /// @dev Raised when the user provides inconsistent arguments.
@@ -72,33 +72,33 @@ contract Infinity is ERC1155 {
         address recipient,
         string calldata message
     ) public payable {
-        uint tokenId = _randomId();
+        uint id = _randomId();
 
-        _generateViaDeposit(recipient, tokenId);
+        _generateViaDeposit(recipient, id);
 
-        _message(recipient, tokenId, message);
+        _message(recipient, id, message);
     }
 
     /// @notice Copy an existing infinity check owned by someone and deposit 0.008 ETH for each token.
     /// @param source The address of an existing owner of the token.
     /// @param recipient The address that should receive the token.
-    /// @param tokenId The token ID to mint.
+    /// @param id The token to mint.
     /// @param message Mint the token with an optional message.
     function generateExisting(
         address source,
         address recipient,
-        uint tokenId,
+        uint id,
         string calldata message
     ) public payable {
-        _validateId(tokenId, source);
+        _validateId(id, source);
 
-        _generateViaDeposit(recipient, tokenId);
+        _generateViaDeposit(recipient, id);
 
-        _message(recipient, tokenId, message);
+        _message(recipient, id, message);
     }
 
     /// @notice Swap an inifinity token for a new one.
-    /// @param id The token ID to burn.
+    /// @param id The token to burn.
     /// @param amount The token amount to burn / recreate.
     function regenerate(uint id, uint amount) public {
         // Execute burn
@@ -109,7 +109,7 @@ contract Infinity is ERC1155 {
     }
 
     /// @notice Destroy the token to withdraw its desposited ETH.
-    /// @param id The token ID to destroy.
+    /// @param id The token to destroy.
     /// @param amount The amount to degenerate (withdraws 0.008 ETH per item).
     function degenerate(
         uint id,
@@ -144,30 +144,30 @@ contract Infinity is ERC1155 {
     /// @notice Copy multiple infinity check tokens and deposit 0.008 ETH in each.
     /// @param sources The addresses of existing owners of each token.
     /// @param recipients The addresses that should receive the token.
-    /// @param tokenIds The tokenIDs to mint.
+    /// @param ids The tokens to mint.
     /// @param amounts The number of tokens to send for each token.
     function generateManyExisting(
         address[] calldata sources,
         address[] calldata recipients,
-        uint[] calldata tokenIds,
+        uint[] calldata ids,
         uint[] calldata amounts
     ) public payable {
         uint count = sources.length;
 
-        _validateCounts(count, recipients.length, tokenIds.length, amounts.length);
+        _validateCounts(count, recipients.length, ids.length, amounts.length);
         _checkDeposit(_totalAmount(amounts));
 
         for (uint i; i < count;) {
-            _validateId(tokenIds[i], sources[i]);
+            _validateId(ids[i], sources[i]);
 
-            _mint(recipients[i], tokenIds[i], amounts[i], "");
+            _mint(recipients[i], ids[i], amounts[i], "");
 
             unchecked { ++i; }
         }
     }
 
     /// @notice Create multiple new infinity check tokens and deposit 0.008 ETH in each.
-    /// @param ids The existing token IDs that should be destroyed in the process.
+    /// @param ids The existing tokens that should be destroyed in the process.
     /// @param amounts The number of tokens to recreate per id.
     function regenerateMany(
         uint[] calldata ids,
@@ -183,7 +183,7 @@ contract Infinity is ERC1155 {
     }
 
     /// @notice Degenerate multiple tokens at once.
-    /// @param ids The tokenIDs to destroy.
+    /// @param ids The tokens to destroy.
     /// @param amounts The amounts to degenerate (withdraws 0.008 ETH per item).
     function degenerateMany(
         uint[] calldata ids,
@@ -197,15 +197,15 @@ contract Infinity is ERC1155 {
     }
 
     /// @notice Render SVG of the token.
-    /// @param tokenId The token ID to render.
-    function svg(uint tokenId) public pure returns (string memory) {
-        return InfiniteArt.renderSVG(InfiniteGenerator.tokenData(tokenId));
+    /// @param id The token to render.
+    function svg(uint id) public pure returns (string memory) {
+        return InfiniteArt.renderSVG(InfiniteGenerator.tokenData(id));
     }
 
     /// @notice Render the encoded token metadata-URI.
-    /// @param tokenId The token ID to get metadata for.
-    function uri(uint tokenId) public pure override returns (string memory) {
-        return InfiniteMetadata.tokenURI(InfiniteGenerator.tokenData(tokenId));
+    /// @param id The token to get metadata for.
+    function uri(uint id) public pure override returns (string memory) {
+        return InfiniteMetadata.tokenURI(InfiniteGenerator.tokenData(id));
     }
 
     /// @notice Supply is (in)finite: (2^256 - 1)^2.
@@ -213,17 +213,17 @@ contract Infinity is ERC1155 {
     function totalSupply(uint) public pure returns (uint) { return type(uint).max; }
 
     /// @dev Mint a token n times, based on the amount of ETH sent.
-    function _generateViaDeposit(address recipient, uint tokenId) internal {
+    function _generateViaDeposit(address recipient, uint id) internal {
         uint amount  = msg.value / price;
         uint surplus = msg.value % price;
 
         if (amount == 0) revert InvalidDesposit();
 
-        _mint(recipient, tokenId, amount, "");
+        _mint(recipient, id, amount, "");
         _send(recipient, surplus);
     }
 
-    /// @dev Validate IDs to minted tokens or randomize for initial mints. Exception for VV mints.
+    /// @dev Make sure the token exists or VV is the minter.
     function _validateId(uint id, address source) internal view {
         bool minted = balanceOf(source, id) > 0;
 
@@ -233,21 +233,21 @@ contract Infinity is ERC1155 {
         revert InvalidToken();
     }
 
-    /// @dev Validate that all counts are equal
+    /// @dev Validate that both counts are equal.
     function _validateCounts(uint a, uint b) internal pure {
         if (a == b) return;
 
         revert InvalidInput();
     }
 
-    /// @dev Validate that all counts are equal
+    /// @dev Validate that all four counts are equal.
     function _validateCounts(uint a, uint b, uint c, uint d) internal pure {
         if (a == b && a == c && a == d) return;
 
         revert InvalidInput();
     }
 
-    /// @dev Make a random generative token ID.
+    /// @dev Make a random generative token.
     function _randomId() internal view returns (uint) {
         return uint(keccak256(abi.encodePacked(block.prevrandao, msg.sender, gasleft())));
     }
@@ -276,9 +276,9 @@ contract Infinity is ERC1155 {
     }
 
     /// @dev Emit a mint message, if provided
-    function _message(address recipient, uint tokenId, string calldata message) internal {
+    function _message(address recipient, uint id, string calldata message) internal {
         if (bytes(message).length > 0) {
-            emit Message(msg.sender, recipient, tokenId, message);
+            emit Message(msg.sender, recipient, id, message);
         }
     }
 }
